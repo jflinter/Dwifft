1dc03cd04f80c55046883d98b69dd32bfc212ad5
Jack Flintermann <jack@stripe.com>
2015-08-23 01:54:33 -0400

    example app

---
+++ DwifftExample/DwifftExample/StuffTableViewController.swift
@@ -0,0 +1,81 @@
+//
+//  StuffTableViewController.swift
+//  DwifftExample
+//
+//  Created by Jack Flintermann on 8/23/15.
+//  Copyright (c) 2015 jflinter. All rights reserved.
+//
+
+import UIKit
+import Dwifft
+
+class StuffTableViewController: UITableViewController {
+
+    static let possibleStuff = [
+        "Cats",
+        "Onions",
+        "A used lobster",
+        "Splinters",
+        "Mud",
+        "Pineapples",
+        "Fish legs",
+        "Duck panties",
+        "Adam's apple",
+        "Igloo cream",
+        "Self-flying car"
+    ]
+    // I shamelessly stole this list of things from my friend Pasquale's blog post because I thought it was funny. You can see it at https://medium.com/elepath-exports/spatial-interfaces-886bccc5d1e9
+    
+    static func randomArrayOfStuff() -> [String] {
+        var possibleStuff = self.possibleStuff
+        for i in 0..<possibleStuff.count - 1 {
+            let j = Int(arc4random_uniform(UInt32(possibleStuff.count - i))) + i
+            swap(&possibleStuff[i], &possibleStuff[j])
+        }
+        let subsetCount: Int = Int(arc4random_uniform(3)) + 5
+        return Array(possibleStuff[0...subsetCount])
+    }
+    
+    required init!(coder aDecoder: NSCoder!) {
+        super.init(coder: aDecoder)
+        self.navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Shuffle", style: .Plain, target: self, action: "shuffle")
+    }
+    
+    @objc func shuffle() {
+        self.stuff = StuffTableViewController.randomArrayOfStuff()
+    }
+    
+    // MARK: - Dwifft stuff
+    // This is the stuff that's relevant to actually using Dwifft. The rest is just boilerplate to get the app working.
+    
+    var diffCalculator: TableViewDiffCalculator<String>?
+    
+    var stuff: [String] = StuffTableViewController.randomArrayOfStuff() {
+        // So, whenever your datasource's array of things changes, just let the diffCalculator know and it'll do the rest.
+        didSet {
+            self.diffCalculator?.rows = stuff
+        }
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.tableView.registerClass(UITableViewCell.self, forCellReuseIdentifier: "reuseIdentifier")
+        self.diffCalculator = TableViewDiffCalculator<String>(tableView: self.tableView, initialRows: self.stuff)
+        
+        // You can change insertion/deletion animations like this! Fade works well. So does Top/Bottom. Left/Right/Middle are a little weird, but hey, do your thing.
+        self.diffCalculator?.insertionAnimation = .Fade
+        self.diffCalculator?.deletionAnimation = .Fade
+    }
+
+    // MARK: - Table view data source
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.stuff.count
+    }
+
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("reuseIdentifier", forIndexPath: indexPath) as! UITableViewCell
+        cell.textLabel?.text = self.stuff[indexPath.row]
+        return cell
+    }
+
+}
---
+++ README.md
@@ -0,0 +1,16 @@
+Dwifft!
+===
+
+OK. Dwifft is a Swift library that does two things. The first thing sounds interesting but perhaps only abstractly useful, and the other thing is a very concretely useful thing based off the first thing.
+
+The first thing (found in `LCS.swift`) is an algorithm that solves the [Longest Common Subsequence problem](https://en.wikipedia.org/wiki/Longest_common_subsequence_problem). Pragmatically, the problem of finding the difference between two arrays is trivially reducable to the LCS problem, i.e. if you can find the longest common subsequence between two arrays, you've also found a series of transforms to apply to array 1 that will result in array 2. This algorithm is written purely in Swift, and uses dynamic programming to achieve substantial performance improvements over a naïve approach (that being said, there are several ways it could probably be sped up.) Perhaps by now you've figured out that Dwifft is a terrible/brilliant portmanteau of "Swift" and "Diff".
+
+The second thing (found in `TableViewDiffCalculator.swift`) is, well, a diff calculator for `UITableView`s. Let's say you have a `UITableView` that's backed by a simple array of values (like a list of names, e.g. `["Alice", "Bob", "Carol"]`. If that array changes (maybe Bob leaves, and is replaced by Dave, so our list is now `["Alice, "Carol", "Dave"]`), we'll want to update the table. The easiest way to do this is by calling `reloadData` on it. This has a couple of downsides: the transition isn't animated, and it'll cause your user to lose their scroll position if they've scrolled the table. The nicer way is to use the `insertRowsAtIndexPaths:withRowAnimation` and `deleteRowsAtIndexPaths:withRowAnimation` methods on `UITableView`, but this requires you to figure out which index paths have changed in your array (in our example, you'd have to figure out that the row at index 1 should be removed, and a new row should be inserted at index 2 should then be added). If only we had a way to diff the previous value of our array with it's new value. Wait a minute.
+
+When you wire up a `TableViewDiffCalculator` to your `UITableView`, it'll _automatically_ calculate diffs and trigger the necessary animations on it whenever you change its `rows` property. Neat, right? Usually, this `rows` object will be the same thing you're using in your `UITableViewDataSource` methods. The only constraint is that the items in that `rows` array have to conform to `Equatable`, because, you know, how else could you compare them?
+
+This makes slightly more sense in code, so check out the example app!
+
+Thanks for reading! PRs and such are of course welcome, but I want to keep this pretty tightly-scoped, so I'd politely request you open an issue before going off and implementing any new functionality so we can talk things over first.
+
+Happy dwiffing!
