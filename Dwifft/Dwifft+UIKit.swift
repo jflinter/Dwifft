9eb295e4d57b52b1c5fbc3429c1dba745d8d8de5
Elliott Williams <e@elliottwillia.ms>
2016-09-16 17:52:52 -0400

    Set a new value for `CollectionViewDiffCalculator`s inside the `performBatchUpdates` method.
    This should fix the bug caused by #21. Now, when a CollectionViewDiffCalculator
    is used in a data source, the data represented by the collection (contained in
    `rows`) will be modified inside `performBatchUpdates`. UIKit expects this and
    produces an NSInternalInconsistencyException otherwise.

--- Dwifft/Dwifft+UIKit.swift
+++ Dwifft/Dwifft+UIKit.swift
@@ -24,7 +24,7 @@ open class TableViewDiffCalculator<T: Equatable> {
     
     /// You can change insertion/deletion animations like this! Fade works well. So does Top/Bottom. Left/Right/Middle are a little weird, but hey, do your thing.
     open var insertionAnimation = UITableViewRowAnimation.automatic, deletionAnimation = UITableViewRowAnimation.automatic
-    
+
     /// Change this value to trigger animations on the table view.
     open var rows : [T] {
         willSet {
@@ -53,24 +53,31 @@ open class CollectionViewDiffCalculator<T: Equatable> {
     
     public init(collectionView: UICollectionView, initialRows: [T] = []) {
         self.collectionView = collectionView
-        self.rows = initialRows
+        _rows = initialRows
     }
     
     /// Right now this only works on a single section of a collectionView. If your collectionView has multiple sections, though, you can just use multiple CollectionViewDiffCalculators, one per section, and set this value appropriately on each one.
     open var sectionIndex: Int = 0
-    
+
+    // Since UICollectionView (unlike UITableView) takes a block which must update its data source and trigger animations, we need to trigger the changes on set, instead of explicitly before and after set. This backing array lets us use a getter/setter in the exposed property.
+    private var _rows: [T]
+
     /// Change this value to trigger animations on the collection view.
     open var rows : [T] {
-        didSet {
-            
-            let oldRows = oldValue
-            let newRows = self.rows
+        get {
+            return _rows
+        }
+        set {
+            let oldRows = rows
+            let newRows = newValue
             let diff = oldRows.diff(newRows)
             if (diff.results.count > 0) {
-                let insertionIndexPaths = diff.insertions.map({ IndexPath(item: $0.idx, section: self.sectionIndex) })
-                let deletionIndexPaths = diff.deletions.map({ IndexPath(item: $0.idx, section: self.sectionIndex) })
-                
                 collectionView?.performBatchUpdates({ () -> Void in
+                    self._rows = newValue
+
+                    let insertionIndexPaths = diff.insertions.map({ IndexPath(item: $0.idx, section: self.sectionIndex) })
+                    let deletionIndexPaths = diff.deletions.map({ IndexPath(item: $0.idx, section: self.sectionIndex) })
+
                     self.collectionView?.insertItems(at: insertionIndexPaths)
                     self.collectionView?.deleteItems(at: deletionIndexPaths)
                 }, completion: nil)
