0eba2a5a706f5bab6a2fe7f5020eb8332df79c76
AleLudovici <alessandro.ludovici@gmail.com>
2017-04-06 15:39:41 +0100

    Use ExpectedAssertion type to assert that the right indexPath is inserted/deleted

--- DwifftTests/CollectionViewDiffTests.swift
+++ DwifftTests/CollectionViewDiffTests.swift
@@ -9,38 +9,50 @@
 import UIKit
 import XCTest
 
+struct ExpectedAssertion<T> {
+    let expectation: XCTestExpectation
+    let assertion: (_ expectedValue: T) -> Void
+}
+
 final class CollectionViewDiffTests: XCTestCase {
     func testCollectionViewDiffCalculator() {
+        var insertionAssertions: [ExpectedAssertion<String>] = []
+        let expectedInsertionIndexes = [0, 3, 4, 5]
 
-
-        var insertionExpectations: [Int: XCTestExpectation] = [:]
-        for i in [0, 3, 4, 5] {
-            let x: XCTestExpectation = expectation(description: "+\(i)")
-            insertionExpectations[i] = x
+        for index in expectedInsertionIndexes {
+            let expected = "\(index)"
+            let insertionExpectation = expectation(description:expected)
+            let insertionAssertion = ExpectedAssertion<String>(expectation: insertionExpectation) { inserted in XCTAssertEqual(inserted, expected) }
+            insertionAssertions.append(insertionAssertion)
         }
 
-        var deletionExpectations: [Int: XCTestExpectation] = [:]
-        for i in [0, 1, 2, 4] {
-            let x: XCTestExpectation = expectation(description: "+\(i)")
-            deletionExpectations[i] = x
+        var deletionAssertions: [ExpectedAssertion<String>] = []
+        let expectedDeletionIndexes = [4, 2, 1, 0]
+
+        for index in expectedDeletionIndexes {
+            let expected = "\(index)"
+            let deletionExpectation = expectation(description:expected)
+            let deletionAssertion = ExpectedAssertion<String>(expectation: deletionExpectation) { deleted in XCTAssertEqual(deleted, expected) }
+            deletionAssertions.append(deletionAssertion)
         }
 
-        let collectionView = TestCollectionView(insertionExpectations: insertionExpectations, deletionExpectations: deletionExpectations)
+        let collectionView = TestCollectionView(insertionAssertions: insertionAssertions, deletionAssertions: deletionAssertions)
         let viewController = TestCollectionViewController(collectionView: collectionView, rows: [0, 1, 2, 5, 8, 9, 0])
+
         viewController.rows = [4, 5, 9, 8, 3, 1, 0]
+
         waitForExpectations(timeout: 1.0, handler: nil)
     }
 }
 
 final class TestCollectionView: UICollectionView {
+    let insertionAssertions: [ExpectedAssertion<String>]
+    let deletionAssertions: [ExpectedAssertion<String>]
 
-    let insertionExpectations: [Int: XCTestExpectation]
-    let deletionExpectations: [Int: XCTestExpectation]
-
-    init(insertionExpectations: [Int: XCTestExpectation], deletionExpectations: [Int: XCTestExpectation]) {
-        self.insertionExpectations = insertionExpectations
-        self.deletionExpectations = deletionExpectations
-        super.init(frame: CGRect.zero, collectionViewLayout: UICollectionViewFlowLayout())
+    init(insertionAssertions: [ExpectedAssertion<String>], deletionAssertions: [ExpectedAssertion<String>]) {
+        self.insertionAssertions = insertionAssertions
+        self.deletionAssertions = deletionAssertions
+        super.init(frame: .zero, collectionViewLayout: UICollectionViewFlowLayout())
     }
 
     required init?(coder aDecoder: NSCoder) {
@@ -49,49 +61,57 @@ final class TestCollectionView: UICollectionView {
 
     override func insertItems(at indexPaths: [IndexPath]) {
         super.insertItems(at: indexPaths)
-        for indexPath in indexPaths {
-            self.insertionExpectations[(indexPath as NSIndexPath).item]!.fulfill()
+
+        indexPaths.enumerated().forEach { index, indexPath in
+            let assertion = insertionAssertions[index]
+            assertion.assertion("\(indexPath.item)")
+            assertion.expectation.fulfill()
         }
     }
 
     override func deleteItems(at indexPaths: [IndexPath]) {
         super.deleteItems(at: indexPaths)
-        for indexPath in indexPaths {
-            self.deletionExpectations[(indexPath as NSIndexPath).item]!.fulfill()
+
+        indexPaths.enumerated().forEach { index, indexPath in
+            let assertion = deletionAssertions[index]
+            assertion.assertion("\(indexPath.item)")
+            assertion.expectation.fulfill()
         }
     }
-
 }
 
 final class TestCollectionViewController: UIViewController, UICollectionViewDataSource {
-
     let testCollectionView: TestCollectionView
     let diffCalculator: CollectionViewDiffCalculator<Int>
+
     var rows: [Int] {
         didSet {
-            self.diffCalculator.rows = rows
+            diffCalculator.rows = rows
         }
     }
 
     init(collectionView: TestCollectionView, rows: [Int]) {
         self.testCollectionView = collectionView
-        self.diffCalculator = CollectionViewDiffCalculator<Int>(collectionView: self.testCollectionView, initialRows: rows)
+        self.diffCalculator = CollectionViewDiffCalculator<Int>(collectionView: collectionView, initialRows: rows)
         self.rows = rows
         super.init(nibName: nil, bundle: nil)
-
-        collectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: "TestCell")
-        collectionView.dataSource = self
+        setupCollectionView()
     }
 
     required init?(coder aDecoder: NSCoder) {
         fatalError("not implemented")
     }
 
-    @objc func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+    private func setupCollectionView() {
+        testCollectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: "TestCell")
+        testCollectionView.dataSource = self
+    }
+
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return diffCalculator.rows.count
     }
 
-    @objc func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         return collectionView.dequeueReusableCell(withReuseIdentifier: "TestCell", for: indexPath)
     }
 }
